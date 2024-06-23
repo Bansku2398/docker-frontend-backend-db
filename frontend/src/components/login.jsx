7dad52a573aa6538d11317bed645e5f73c2f363f
Cikatricis <michal.cikatricis@accenture.com>
2024-06-23 23:28:25 +0200

    fix

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const apiUri = process.env.API_URL;
+const apiUri = process.env.REACT_APP_API_URL || process.env.API_URL || "https://paas-backend1.azurewebsites.net/api";
 
 const Login = () => {
   let loading = false;
